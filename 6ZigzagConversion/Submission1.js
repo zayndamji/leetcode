d97ed690ae675d3074cc0d2b910e3c982e9eec6e
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-13 14:40:01 -0700

    Problem 6 - Zigzag Conversion (JS)

---
+++ 6ZigzagConversion/Submission1.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/zigzag-conversion/
+// Language: JavaScript
+// Runtime: 78 MS
+// Memory: 53.88 MB
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows == 1) return s;
+
+  let str = "";
+
+  for (let i = 0; i < s.length; ) {
+    str += s[i];
+    i += (numRows-1)*2;
+  }
+
+  for (let rowNum = 1; rowNum < numRows-1; rowNum++) {
+    for (let i = rowNum; i < s.length; ) {
+      str += s[i];
+      i += ((numRows-rowNum)-1)*2;
+      if (i < s.length) {
+        str += s[i];
+        i += rowNum*2;
+      }
+    }
+  }
+
+  for (let i = numRows-1; i < s.length; ) {
+    str += s[i];
+    i += (numRows-1)*2;
+  }
+
+  return str;
+};
