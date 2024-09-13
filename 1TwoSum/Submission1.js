b36f717d4a61940d33b19e829aa76c216bed6dbf
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-13 09:55:15 -0700

    Problem 1 - Two Sum (JS)

---
+++ 1TwoSum/Submission1.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/two-sum/
+// Language: JavaScript
+// Runtime: 100 MS
+// Memory: 49.64 MB
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+};
