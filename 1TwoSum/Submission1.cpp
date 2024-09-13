f4f6a98ec3aca41b0f387d5f48216d25ee829c20
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-13 13:18:48 -0700

    Problem 1 - Two Sum (C++)

---
+++ 1TwoSum/Submission1.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/two-sum/
+// Language: C++
+// Runtime: 48 MS
+// Memory: 12.97 MB
+
+#include "vector"
+
+class Solution {
+public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    int iSize = nums.size()-1;
+    for (int i = 0; i < iSize; i++) {
+      int jSize = nums.size();
+      for (int j = i + 1; j < jSize; j++) {
+        if (nums[i] + nums[j] == target) {
+          return std::vector<int>{i, j};
+        }
+      }
+    }
+
+    return std::vector<int>{};
+  }
+};
