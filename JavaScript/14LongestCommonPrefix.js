05af97e23c99faeb6909db5b26153f5d0b667030
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-16 09:40:43 -0700

    Problem 14 - Longest Common Prefix (JS)

---
+++ JavaScript/14LongestCommonPrefix.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-common-prefix/
+// Language: JavaScript
+// Runtime: 61 MS
+// Memory: 50.44 MB
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let str = strs[0];
+
+  for (let i = 0; i < strs.length; i++) {
+    let j = 0;
+    for (; j < Math.min(strs[i].length, str.length); j++) {
+      if (strs[i][j] != str[j]) break;
+    }
+    
+    str = str.substring(0, j);
+  }
+
+  return str;
+};
