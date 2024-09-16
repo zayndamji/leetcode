4d4921904216ad0f058e376ff6a2c44f452bdccd
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-16 13:20:07 -0700

    fixed spacing

--- JavaScript/17LetterCombinationsOfAPhoneNumber.js
+++ JavaScript/17LetterCombinationsOfAPhoneNumber.js
@@ -9,46 +9,46 @@ var letterCombinations = function(digits) {
   if (digits.length == 0) return [];
 
   const letters = {
-      '2': 'abc',
-      '3': 'def',
-      '4': 'ghi',
-      '5': 'jkl',
-      '6': 'mno',
-      '7': 'pqrs',
-      '8': 'tuv',
-      '9': 'wxyz'
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
   }
 
   const combinations = [];
 
   if (digits.length == 1) {
-      for (let i = 0; i < letters[digits[0]].length; i++) {
-          combinations.push(letters[digits[0]][i]);
-      }
+    for (let i = 0; i < letters[digits[0]].length; i++) {
+      combinations.push(letters[digits[0]][i]);
+    }
   } else if (digits.length == 2) {
-      for (let i = 0; i < letters[digits[0]].length; i++) {
-          for (let j = 0; j < letters[digits[1]].length; j++) {
-              combinations.push(letters[digits[0]][i] + letters[digits[1]][j]);
-          }
+    for (let i = 0; i < letters[digits[0]].length; i++) {
+      for (let j = 0; j < letters[digits[1]].length; j++) {
+        combinations.push(letters[digits[0]][i] + letters[digits[1]][j]);
       }
+    }
   } else if (digits.length == 3) {
-      for (let i = 0; i < letters[digits[0]].length; i++) {
-          for (let j = 0; j < letters[digits[1]].length; j++) {
-              for (let k = 0; k < letters[digits[2]].length; k++) {
-                  combinations.push(letters[digits[0]][i] + letters[digits[1]][j] + letters[digits[2]][k]);
-              }
-          }
+    for (let i = 0; i < letters[digits[0]].length; i++) {
+      for (let j = 0; j < letters[digits[1]].length; j++) {
+        for (let k = 0; k < letters[digits[2]].length; k++) {
+          combinations.push(letters[digits[0]][i] + letters[digits[1]][j] + letters[digits[2]][k]);
+        }
       }
+    }
   } else if (digits.length == 4) {
-      for (let i = 0; i < letters[digits[0]].length; i++) {
-          for (let j = 0; j < letters[digits[1]].length; j++) {
-              for (let k = 0; k < letters[digits[2]].length; k++) {
-                  for (let l = 0; l < letters[digits[3]].length; l++) {
-                      combinations.push(letters[digits[0]][i] + letters[digits[1]][j] + letters[digits[2]][k] + letters[digits[3]][l]);
-                  }
-              }
+    for (let i = 0; i < letters[digits[0]].length; i++) {
+      for (let j = 0; j < letters[digits[1]].length; j++) {
+        for (let k = 0; k < letters[digits[2]].length; k++) {
+          for (let l = 0; l < letters[digits[3]].length; l++) {
+            combinations.push(letters[digits[0]][i] + letters[digits[1]][j] + letters[digits[2]][k] + letters[digits[3]][l]);
           }
+        }
       }
+    }
   }
 
   return combinations;
--- JavaScript/4MedianOfTwoSortedArrays.js
+++ JavaScript/4MedianOfTwoSortedArrays.js
@@ -7,21 +7,21 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let arr = [], index1 = 0, index2 = 0;
-    for (let i = 0; i < nums1.length+nums2.length; i++) {
-      if (nums1[index1] < nums2[index2] 
-          || index2 == nums2.length) {
-        arr.push(nums1[index1]);
-        index1++;
-      } else {
-        arr.push(nums2[index2]);
-        index2++;
-      }
+  let arr = [], index1 = 0, index2 = 0;
+  for (let i = 0; i < nums1.length+nums2.length; i++) {
+    if (nums1[index1] < nums2[index2] 
+        || index2 == nums2.length) {
+      arr.push(nums1[index1]);
+      index1++;
+    } else {
+      arr.push(nums2[index2]);
+      index2++;
     }
+  }
 
-    if (arr.length % 2 == 0) {
-      return (arr[arr.length/2] +  arr[arr.length/2-1]) / 2;
-    }
+  if (arr.length % 2 == 0) {
+    return (arr[arr.length/2] +  arr[arr.length/2-1]) / 2;
+  }
 
-    return arr[Math.floor(arr.length/2)];
+  return arr[Math.floor(arr.length/2)];
 };
