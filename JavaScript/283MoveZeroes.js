2596fc4cdcd3c2eec2b197e98f8c9aa20362f0ac
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-17 13:26:55 -0700

    Problem 283 - Move Zeroes (JS)

---
+++ JavaScript/283MoveZeroes.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/move-zeroes/
+// Language: JavaScript
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      let j = i+1;
+      while (j < nums.length) {
+        if (nums[j] != 0) break;
+        j++;
+      }
+
+      if (j < nums.length) {
+        nums[i] = nums[j];
+        nums[j] = 0;
+      } else {
+        break;
+      }
+    }
+  }
+};
