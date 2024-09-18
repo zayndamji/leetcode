0678a1e8e1267da6a60424bb9cd1da70e4f2745d
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-18 13:33:12 -0700

    Problem 19 - Remove Nth Node from End of List (JS)

---
+++ JavaScript/19RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// Language: JavaScript
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  let len = 1;
+  let cur = head;
+  while (cur.next) {
+    len += 1;
+    cur = cur.next;
+  }
+
+  cur = head;
+  let m = len - n;
+
+  if (m == 0) {
+    if (head.next) {
+      return head.next;
+    } else {
+      return null;
+    }
+  }
+
+  let i = 0;
+  while (i < (m-1)) {
+    cur = cur.next;
+    i++;
+  }
+
+  if (cur.next) {
+    if (cur.next.next) {
+      cur.next = cur.next.next;
+    } else {
+      cur.next = null;
+    }
+  } else {
+    return null;
+  }
+
+  return head;
+};
