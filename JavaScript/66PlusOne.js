6f155f9e55e371570a18bbd791579d13a6b854f7
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-18 09:10:31 -0700

    Problem 66 - Plus One (JS)

---
+++ JavaScript/66PlusOne.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/plus-one/
+// Language: JavaScript
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let index = -1;
+  digits[digits.length+index] += 1;
+
+  while (digits[digits.length+index] > 9) {
+    digits[digits.length+index] = 0;
+    if (digits[digits.length+index-1] == undefined) {
+      digits.unshift(1);
+    } else {
+      digits[digits.length+index-1] += 1;
+    }
+    index -= 1;
+  }
+
+  return digits;
+};
