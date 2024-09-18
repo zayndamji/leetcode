782535b0fdd32d0522911a570e2c8ee8a8b7ca5e
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-18 13:19:53 -0700

    Problem 20 - Valid Parenthesis (JS)

---
+++ JavaScript/20ValidParenthesis.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/valid-parentheses/
+// Language: JavaScript
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+      stack.push(s[i]);
+    } else {
+      if (stack.length == 0)
+        return false;
+
+      const last = stack.pop();
+      if ((last == '(' && s[i] != ')') ||
+          (last == '{' && s[i] != '}') ||
+          (last == '[' && s[i] != ']')) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length > 0)
+    return false;
+
+  return true;
+};
