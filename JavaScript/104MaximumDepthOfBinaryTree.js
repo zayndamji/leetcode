460a0002b53ffa9b868b72abd74262b7c543fdaa
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-19 09:57:13 -0700

    Problem 104 - Maximum Depth of Binary Tree (JS)

---
+++ JavaScript/104MaximumDepthOfBinaryTree.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// Language: JavaScript
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == null) return 0;
+  
+  if (root.left == null && root.right == null) return 1;
+
+  if (root.left == null) return maxDepth(root.right) + 1;
+  if (root.right == null) return maxDepth(root.left) + 1;
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
