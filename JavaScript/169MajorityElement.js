b37b17f157de411cfc045698762992b5f12f6796
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-19 14:46:12 -0700

    Problem 169 - Majority Element (JS)

---
+++ JavaScript/169MajorityElement.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/majority-element/
+// Language: JavaScript
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], map.get(nums[i]) ? map.get(nums[i])+1 : 1);
+    if (map.get(nums[i]) > nums.length/2) return nums[i];
+  }
+};
