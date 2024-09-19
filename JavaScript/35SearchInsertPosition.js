040598389d27f92039e8abdd93cccca84c87c932
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-18 19:11:05 -0700

    Problem 35 - Search Insert Position (JS)

---
+++ JavaScript/35SearchInsertPosition.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/search-insert-position/
+// Language: JavaScript
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  return binarySearch(nums, target, 0, nums.length-1);
+};
+
+var binarySearch = function(nums, target, startIndex, endIndex) {
+  if (startIndex >= endIndex) {
+    if (nums[startIndex] < target) {
+      return startIndex+1;
+    } else {
+      return startIndex;
+    }
+  }
+
+  const midIndex = Math.floor(startIndex + ((endIndex-startIndex) / 2));
+  const midNum = nums[midIndex];
+
+  if (midNum == target) {
+    return midIndex;
+  }
+
+  if (midNum > target) {
+    return binarySearch(nums, target, startIndex, midIndex-1);
+  }
+
+  return binarySearch(nums, target, midIndex+1, endIndex);
+}
