462730d87270671e2ac2e904f89a982e0d9332c3
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-21 08:44:16 -0700

    commented problem 125

--- JavaScript/125ValidPalindrome.js
+++ JavaScript/125ValidPalindrome.js
@@ -6,11 +6,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+  // remove non-alphanumeric characters, convert to lowercase to ignore case
   s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
   
   for (let i = 0; i < Math.floor(s.length/2); i++) {
+    // if position i doesn't equal the character at -i, the string must not be a palindrome, so return false
     if (s[i] != s[s.length-i-1]) return false;
   }
 
+  // string must be a palindrome, so return true
   return true;
 };
