43fdeb75c54b80f9240ee576d3aa0b2e64555504
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-21 08:47:53 -0700

    Problem 144 - Binary Tree Preorder Traversal (JS)

---
+++ JavaScript/144BinaryTreePreorderTraversal.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+// Language: JavaScript
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  if (root == null)
+    return [];
+
+  return [root.val].concat(preorderTraversal(root.left)).concat(preorderTraversal(root.right));
+};
