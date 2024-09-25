a9fe1d1701a0fb59fee666c1c742e601958608a6
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-25 09:56:04 -0700

    Problem 268 - Missing Number (JS)

---
+++ JavaScript/268MissingNumber.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/missing-number/
+// Language: JavaScript
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i != nums[i]) return i;
+  }
+
+  return nums.length;
+};
