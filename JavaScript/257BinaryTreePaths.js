9d77b5cb87feec65283a1fb21225607099e48d44
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-25 09:28:40 -0700

    Problem 257 - Binary Tree Paths (JS)

---
+++ JavaScript/257BinaryTreePaths.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-paths/
+// Language: JavaScript
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  return binaryTreeRecursive(root, "");
+};
+
+var binaryTreeRecursive = function(root, str) {
+  if (root == null) return [];
+
+  if (root.left == null && root.right == null) {
+    return [str+root.val];
+  }
+
+  if (root.left == null) return binaryTreeRecursive(root.right, str+root.val+"->");
+
+  if (root.right == null) return binaryTreeRecursive(root.left, str+root.val+"->");
+
+  return binaryTreeRecursive(root.left, str+root.val+"->").concat(binaryTreeRecursive(root.right, str+root.val+"->"));
+}
