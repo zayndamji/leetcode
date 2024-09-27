65bebdd0d08dcc1668e47baa4e1a51f299e9dde4
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-27 13:39:35 -0700

    Problem 136 - Single Number (Submission 2 JS)

--- JavaScript/136SingleNumber.js
+++ JavaScript/136SingleNumber.js
@@ -1,5 +1,11 @@
 // https://leetcode.com/problems/single-number/
 
+/*
+Submission 1:
+Time Complexity O(N^2)
+Space Complexity O(1)
+*/
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -15,4 +21,33 @@ var singleNumber = function(nums) {
   }
 
   return nums[i];
+};
+
+/*
+Submission 2:
+Time Complexity O(N)
+Space Complexity O(N)
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  const numMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numMap.get(nums[i]) != undefined) {
+      numMap.set(nums[i], numMap.get(nums[i])+1);
+    } else {
+      numMap.set(nums[i], 1);
+    }
+  }
+
+  const numMapEntries = Array.from(numMap.entries());
+  for (let i = 0; i < numMapEntries.length; i++) {
+    if (numMapEntries[i][1] == 1) {
+      return numMapEntries[i][0];
+    }
+  }
 };
