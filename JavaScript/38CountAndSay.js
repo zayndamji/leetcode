ad810339f7d513578e0fa4cdf92d131536adda74
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-09-27 09:53:40 -0700

    Problem 38 - Count and Say (JS)

---
+++ JavaScript/38CountAndSay.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/count-and-say/
+// Language: JavaScript
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+  if (n == 1) return "1";
+
+  let rle = countAndSay(n-1);
+  let str = "", iter = 0;
+  for (let i = 0; i < rle.length; i++) {
+    iter++;
+    if (i == rle.length-1 || rle[i] != rle[i+1]) {
+      str += "" + iter + rle[i];
+      iter = 0;
+    }
+  }
+
+  return str;
+};
