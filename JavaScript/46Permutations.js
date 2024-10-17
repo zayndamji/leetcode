793ff3f62dac0b46bc863485e7ce3c2a19f777d8
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-10-17 11:31:59 -0700

    Problem 46 - Permutations

---
+++ JavaScript/46Permutations.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/permutations/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  if (nums.length == 1) return [[nums[0]]];
+
+  const permutations = [];
+  for (let i = 0; i < nums.length; i++) {
+    const permuteRes = permute(nums.slice(0, i).concat(nums.slice(i+1)))
+    for (let j = 0; j < permuteRes.length; j++) {
+      permutations.push([nums[i]].concat(permuteRes[j]));
+    }
+  }
+  return permutations;
+};
